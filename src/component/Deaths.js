742395d1b8c572347ad97951e7016139b790581d
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-28 13:37:50 +0100

    Fix all errors

--- src/component/Deaths.js
+++ src/component/Deaths.js
@@ -1,10 +1,8 @@
-const Deaths = () => {
-  return (
-    <div>
-      <h3>Country</h3>
-      <p>Number of deaths</p>
-    </div>
-  );
-};
+const Deaths = () => (
+  <div>
+    <h3>Country</h3>
+    <p>Number of deaths</p>
+  </div>
+);
 
 export default Deaths;
--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -1,11 +1,9 @@
-const Header = () => {
-  return (
-    <div>
-      <h1>Number of death(Covid-!19)</h1>
-      <h2>Search</h2>
-      <input type="text" placehlder="Country" />
-    </div>
-  );
-};
+const Header = () => (
+  <div>
+    <h1>Number of death(Covid-!19)</h1>
+    <h2>Search</h2>
+    <input type="text" placehlder="Country" />
+  </div>
+);
 
 export default Header;
