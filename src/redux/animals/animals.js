99e042d9f633a4e6d433695a1f58f08757894a9c
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-29 17:29:25 +0100

    add reducer

--- src/redux/animals/animals.js
+++ src/redux/animals/animals.js
@@ -1,28 +1,49 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getAnimal } from '../redux/animals/animals';
-import SinglAnimal from './SinglAnimal';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import API_URL from '../../api/api';
 
-const Animals = () => {
-  const dispatch = useDispatch();
-  const animals = useSelector((state) => state.animal);
-  useEffect(() => {
-    dispatch(getAnimal());
-  }, [dispatch]);
+const ADD_ANIMAL = 'zoo/animals/ADD_ANIMAL';
 
-  return (
-    <div>
-      {animals.map((animal) => (
-        <SinglAnimal
-          key={animal.id}
-          id={animal.id}
-          name={animal.name}
-          latinName={animal.latinName}
-          image={animal.image}
-        />
-      ))}
-    </div>
-  );
+const initialState = [];
+
+export default function animals(state = initialState, action) {
+  switch (action.type) {
+    case 'zoo/animals/ADD_ANIMAL/fulfilled':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const duplicateData = (data) => {
+  const changed = [];
+
+  data.forEach((obj) => {
+    const newObj = {
+      id: obj.id,
+      name: obj.name,
+      latinName: obj.latin_name,
+      image: obj.image_link,
+      type: obj.animal_type,
+      active: obj.active_time,
+      minWeigth: obj.weight_min,
+      maxWeigth: obj.weight_max,
+      minLength: obj.length_min,
+      maxLength: obj.length_max,
+      life: obj.lifespan,
+      habitat: obj.habitat,
+      diet: obj.diet,
+      geoRange: obj.geo_range,
+    };
+    changed.push(newObj);
+  });
+  return changed;
 };
 
-export default Animals;
+const getAnimal = createAsyncThunk(ADD_ANIMAL, async () => {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  const altered = duplicateData(data);
+  return altered;
+});
+
+export { getAnimal };
