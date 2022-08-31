83e828122a14e7161e1263444b4a91dc3478dd56
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-08-31 13:10:48 +0100

    Final styles changes

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -3,11 +3,11 @@ import './Header.scss';
 const Header = () => (
   <div className="header">
     <h1>Animal ZOO</h1>
-    <div>
-      <h2 className="search">Search</h2>
-      <input className="input" type="text" placehlder="Country" />
-    </div>
   </div>
 );
 
 export default Header;
+// <div>
+//   // <h2 className="search">Search</h2>
+//   // <input className="input" type="text" placehlder="Country" />
+// </div>
