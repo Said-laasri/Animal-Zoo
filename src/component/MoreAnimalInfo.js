44be42c42e25c0e957c2da80928b098346bdfeb4
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-01 08:10:59 +0100

    Delete console

--- src/component/MoreAnimalInfo.js
+++ src/component/MoreAnimalInfo.js
@@ -24,8 +24,6 @@ const MoreAnimalInfo = () => {
   } = location.state;
   const props = animals.find((item) => item.id === id);
 
-  console.log(props);
-
   return (
     <div className="d-flex">
       <NavLink to="/">
@@ -41,64 +39,34 @@ const MoreAnimalInfo = () => {
             {type}
           </li>
           <li>
-            <h5>Active-Time:</h5>
-            {' '}
-&nbsp;
+            <h5>Active-Time:</h5> &nbsp;
             {active}
           </li>
           <li>
-            <h5>Weigth:</h5>
-            {' '}
-&nbsp;
-            {minWeigth}
-            {' '}
-&nbsp; lbs &nbsp;- &nbsp;
-            {maxWeigth}
-            {' '}
-&nbsp; lbs
+            <h5>Weigth:</h5> &nbsp;
+            {minWeigth} &nbsp; lbs &nbsp;- &nbsp;
+            {maxWeigth} &nbsp; lbs
           </li>
           <li>
-            <h5>Length:</h5>
-            {' '}
-&nbsp;
-            {minLength}
-            {' '}
-&nbsp; &nbsp;ft &nbsp;- &nbsp;
-            {maxLength}
-            {' '}
-&nbsp; ft
+            <h5>Length:</h5> &nbsp;
+            {minLength} &nbsp; &nbsp;ft &nbsp;- &nbsp;
+            {maxLength} &nbsp; ft
           </li>
           <li>
-            <h5>Lifespan:</h5>
-            {' '}
-&nbsp;
-            {life}
-            {' '}
-&nbsp; years
+            <h5>Lifespan:</h5> &nbsp;
+            {life} &nbsp; years
           </li>
           <li>
-            <h5>Habitat:</h5>
-            {' '}
-&nbsp;
-            {habitat}
-            {' '}
-&nbsp;
+            <h5>Habitat:</h5> &nbsp;
+            {habitat} &nbsp;
           </li>
           <li>
-            <h5>Diet:</h5>
-            {' '}
-&nbsp;
-            {diet}
-            {' '}
-&nbsp;
+            <h5>Diet:</h5> &nbsp;
+            {diet} &nbsp;
           </li>
           <li>
-            <h5>GeoLocation:</h5>
-            {' '}
-&nbsp;
-            {geoRange}
-            {' '}
-&nbsp;
+            <h5>GeoLocation:</h5> &nbsp;
+            {geoRange} &nbsp;
           </li>
         </ul>
       </article>
